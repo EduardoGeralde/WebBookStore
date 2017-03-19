ea94a9392f95fc5f5f9172ddc4231ad40fc7ddb6
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-03-19 15:07:53 -0300

    First controller class

---
+++ src/main/java/com/eduardoportfolio/store/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.eduardoportfolio.store.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+	
+	public void index(){
+		//Here we'll load the products
+	}
+
+}
