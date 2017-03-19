53dad38a1bec87cd0d94e5019863b285fc3e9fb5
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-03-19 17:26:00 -0300

    comments

--- src/main/java/com/eduardoportfolio/store/controllers/HomeController.java
+++ src/main/java/com/eduardoportfolio/store/controllers/HomeController.java
@@ -3,14 +3,19 @@ package com.eduardoportfolio.store.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//@Controller annotation tells SpringMVC that this class , is effectively, the responsible to meet 
+//requests from a client(browser).
 @Controller
 public class HomeController {
 	
+	//Tell MVC which URL this method should respond (Called Binding).
 	@RequestMapping("/")
 	public String index(){
 		//Here we'll load the products
 		System.out.println("Loading products");
 		return "hello-world.jsp";
 	}
+	
+	
 
 }
