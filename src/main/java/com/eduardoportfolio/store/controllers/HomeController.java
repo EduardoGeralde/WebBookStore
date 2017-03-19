5a73997bc585cdf8d1f08d69006cc6396eae6209
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-03-19 15:23:57 -0300

    Binding index method and System out println

--- src/main/java/com/eduardoportfolio/store/controllers/HomeController.java
+++ src/main/java/com/eduardoportfolio/store/controllers/HomeController.java
@@ -1,12 +1,15 @@
 package com.eduardoportfolio.store.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
+	@RequestMapping("/home")
 	public void index(){
 		//Here we'll load the products
+		System.out.println("Loading products");
 	}
 
 }
