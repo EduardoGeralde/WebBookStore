bedc8f368206477c35e9da205dfb750995749d9f
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-03-20 17:07:11 -0300

    comments

--- src/main/java/com/eduardoportfolio/store/dao/ProductDao.java
+++ src/main/java/com/eduardoportfolio/store/dao/ProductDao.java
@@ -8,9 +8,17 @@ import org.springframework.stereotype.Repository;
 
 import com.eduardoportfolio.store.models.Product;
 
+/**
+ * 
+ * @author Eduardo Geralde Neto
+ * 
+ */
+
+//Indicate that this class besides being managed for the Spring, it is responsible for the data access.
 @Repository
 public class ProductDao {
 
+	//Indicates Injection of Entity Manager 
 	@PersistenceContext
 	private EntityManager manager;
 	
