08f09769c4aff0f4097b39c4cc03d1ba0c95ffab
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-03-19 22:44:39 -0300

    Object Product passed as parameter'

--- src/main/java/com/eduardoportfolio/store/controllers/ProductsController.java
+++ src/main/java/com/eduardoportfolio/store/controllers/ProductsController.java
@@ -3,12 +3,14 @@ package com.eduardoportfolio.store.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.eduardoportfolio.store.models.Product;
+
 @Controller
 public class ProductsController {
 	
 	@RequestMapping("/products")
-	public String save() {
-		System.out.println("Registering the product");
+	public String save(Product product) {
+		System.out.println("Registering the product"+product);
 		return "products/ok";
 	}
 }
