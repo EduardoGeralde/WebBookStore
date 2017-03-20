1b4735f95f06b4fdb05ff5230edc4fc4c615c313
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-03-19 22:01:28 -0300

    Product Model created

---
+++ src/main/java/com/eduardoportfolio/store/models/Product.java
@@ -0,0 +1,30 @@
+package com.eduardoportfolio.store.models;
+
+public class Product {
+	
+	private String title;
+	private String descrption;
+	private int pages;
+	
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescrption() {
+		return descrption;
+	}
+	public void setDescrption(String descrption) {
+		this.descrption = descrption;
+	}
+	public int getPages() {
+		return pages;
+	}
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+	
+	
+}
