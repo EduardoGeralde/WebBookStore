5d534a338406b6acaba3f82b565be78ccfc831b4
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-03-28 14:34:22 -0300

    comments

--- src/main/java/com/eduardoportfolio/store/models/BookType.java
+++ src/main/java/com/eduardoportfolio/store/models/BookType.java
@@ -1,5 +1,13 @@
 package com.eduardoportfolio.store.models;
 
+/**
+ * 
+ * @author Eduardo Geralde Neto
+ * 
+ * Enum class, representing all our book types.
+ * 
+ */
+
 public enum BookType {
 	
 	EBOOK,PRINTED,COMBO
