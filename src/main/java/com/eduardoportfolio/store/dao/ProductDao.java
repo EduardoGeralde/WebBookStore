2970530f9f0bfd8fbeedc0b761a5d2cffa6a5b17
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-04-01 22:11:46 -0300

    find method inserted

--- src/main/java/com/eduardoportfolio/store/dao/ProductDao.java
+++ src/main/java/com/eduardoportfolio/store/dao/ProductDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 
@@ -33,5 +34,13 @@ public class ProductDao {
 	public List<Product> list(){
 		return manager.createQuery("select distinct (p) from Product p join fetch p.prices",
 									Product.class).getResultList();
+	}
+	
+	public Product find(Integer id) {
+		TypedQuery<Product> query = manager
+				.createQuery(
+						"select distinct(p) from Product p join fetch p.prices where p.id=:id",
+						Product.class).setParameter("id", id);
+		return query.getSingleResult();
 	}
 }
