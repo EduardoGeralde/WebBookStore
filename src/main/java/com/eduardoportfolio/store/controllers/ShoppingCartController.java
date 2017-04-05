61eac8bd09cffca6a405bf7d0bb017083394ed61
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-04-04 22:16:55 -0300

    delete Request scope annotation, because we are using proxyMode in model class

--- src/main/java/com/eduardoportfolio/store/controllers/ShoppingCartController.java
+++ src/main/java/com/eduardoportfolio/store/controllers/ShoppingCartController.java
@@ -1,11 +1,9 @@
 package com.eduardoportfolio.store.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.eduardoportfolio.store.dao.ProductDao;
@@ -16,7 +14,6 @@ import com.eduardoportfolio.store.models.ShoppingItem;
 
 @Controller
 @RequestMapping("/shopping")
-@Scope(value=WebApplicationContext.SCOPE_REQUEST)
 public class ShoppingCartController {
 	
 	@Autowired
