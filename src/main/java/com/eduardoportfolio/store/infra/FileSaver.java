87bceefcdb506a7c02afbb000cf73d12cd7e0676
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-04-06 10:20:09 -0300

    removed unused imports

--- src/main/java/com/eduardoportfolio/store/infra/FileSaver.java
+++ src/main/java/com/eduardoportfolio/store/infra/FileSaver.java
@@ -1,20 +1,13 @@
 package com.eduardoportfolio.store.infra;
 
-import java.io.File;
 import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.AmazonClientException;
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.S3ClientOptions;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
 @Component
