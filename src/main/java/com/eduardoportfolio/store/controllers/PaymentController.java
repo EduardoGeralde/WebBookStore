f245e63dcf3674d01d5d777e87f3e2ab11a20059
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-04-06 15:13:21 -0300

    integration code implanted

--- src/main/java/com/eduardoportfolio/store/controllers/PaymentController.java
+++ src/main/java/com/eduardoportfolio/store/controllers/PaymentController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
+import com.eduardoportfolio.store.models.PaymentData;
 import com.eduardoportfolio.store.models.ShoppingCart;
 
 @Controller
@@ -15,12 +18,20 @@ public class PaymentController {
 
 	@Autowired
 	private ShoppingCart shoppingCart;
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	@RequestMapping(value="checkout" , method=RequestMethod.POST)
 	public String checkout(){
 		BigDecimal total = shoppingCart.getTotal();
 		//integration code
+		String uriToPay = "http://book-payment.herokuapp.com/payment";
+		try {
+			String response = restTemplate.postForObject(uriToPay, new PaymentData(total), String.class);
 		return "redirect:/success";
+		} catch (HttpClientErrorException exception) {
+		return "redirect:/payment/error";
+		}
 	}
-	
 }
+
