3f6b3e9e60bee70351d5ca856a4713300de0fd94
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-04-09 10:02:37 -0300

    same as before

--- src/main/java/com/eduardoportfolio/store/service/PaymentIntegration.java
+++ src/main/java/com/eduardoportfolio/store/service/PaymentIntegration.java
@@ -1,5 +1,36 @@
 package com.eduardoportfolio.store.service;
 
-public class PaymentIntegration {
+import java.math.BigDecimal;
+
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import com.eduardoportfolio.store.models.PaymentData;
+
+public class PaymentIntegration implements Runnable{
+	
+	private DeferredResult<String> result;
+	private BigDecimal value;
+	private RestTemplate restTemplate;
+	
+	public PaymentIntegration(DeferredResult<String> result, BigDecimal value, RestTemplate restTemplate){
+		super();
+		this.result = result;
+		this.value = value;
+		this.restTemplate = restTemplate;
+	}
+	
+	@Override
+	public void run(){
+		String uriToPay = "http://localhost:9000/payment";
+		try{
+			String response = restTemplate.postForObject(uriToPay, new PaymentData(value),String.class);
+			//notification line
+			result.setResult("redirect:/payment/success");
+		} catch (HttpClientErrorException exception){
+			result.setResult("redirect:/payment/error");
+		}
+	}
 
 }
