fcde4556e1b5f6ed4ef7fa9a6e3d70a7fccbf788
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-04-09 22:26:48 -0300

    cacheable annotation inserted in the list method

--- src/main/java/com/eduardoportfolio/store/controllers/ProductsController.java
+++ src/main/java/com/eduardoportfolio/store/controllers/ProductsController.java
@@ -4,6 +4,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -74,7 +75,8 @@ public class ProductsController {
 		redirectAttributes.addFlashAttribute("success","Product successfully registered");
 		return new ModelAndView("redirect:products");
 	}
-	 
+	
+	@Cacheable
 	@RequestMapping(method=RequestMethod.GET)
 	public ModelAndView list(){
 		ModelAndView modelAndView = new ModelAndView ("products/list");
