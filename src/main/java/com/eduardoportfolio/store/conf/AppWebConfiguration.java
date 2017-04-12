26fc52f609ffccebe0ec52292692262db5ec8133
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-04-11 21:26:58 -0300

    contenNegotiatingViewResolver created

--- src/main/java/com/eduardoportfolio/store/conf/AppWebConfiguration.java
+++ src/main/java/com/eduardoportfolio/store/conf/AppWebConfiguration.java
@@ -1,5 +1,7 @@
 package com.eduardoportfolio.store.conf;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.cache.CacheManager;
@@ -13,17 +15,21 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.eduardoportfolio.store.controllers.HomeController;
 import com.eduardoportfolio.store.dao.ProductDao;
 import com.eduardoportfolio.store.infra.FileSaver;
 import com.eduardoportfolio.store.models.ShoppingCart;
+import com.eduardoportfolio.store.viewresolver.JsonViewResolver;
 import com.google.common.cache.CacheBuilder;
 
 /**
@@ -68,6 +74,7 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter{
 	*/
 	
 	@Bean
+	//Guava cache implementation
 	public CacheManager cacheManager(){
 		CacheBuilder<Object,Object> builder = CacheBuilder.newBuilder().maximumSize(100)
 															.expireAfterAccess(5, TimeUnit.MINUTES);
@@ -107,6 +114,19 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter{
 	@Bean
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
+	}
+	
+	@Bean
+	//Create a ViewResolver list (XML, JSON, etc) and pass to a ContentNegotiatingViewResolver object
+	public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager){
+		List<ViewResolver> resolvers = new ArrayList<ViewResolver>();
+		resolvers.add(internalResourceViewResolver());
+		resolvers.add(new JsonViewResolver());
+		
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setViewResolvers(resolvers);
+		resolver.setContentNegotiationManager(manager);
+		return resolver;
 	}
 }
 
