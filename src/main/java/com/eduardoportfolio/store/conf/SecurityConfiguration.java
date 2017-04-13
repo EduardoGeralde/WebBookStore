beadc5da5e5ab0850c38565e17a06c2e9ebabd01
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-04-13 11:56:10 -0300

    comments

--- src/main/java/com/eduardoportfolio/store/conf/SecurityConfiguration.java
+++ src/main/java/com/eduardoportfolio/store/conf/SecurityConfiguration.java
@@ -3,7 +3,12 @@ package com.eduardoportfolio.store.conf;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+//Should be placed in top of Spring Security Configuration classes responsible for effective handle
+//the access rules.
 @EnableWebSecurity
+//We inherit WebSecurityConfigurerAdapter class that already provide all infrastructure ready to start 
+//our security configurations. This annotation also loads other components, like SecurityExpressionLanguage
+//and WebInvocationPrivilegeEvaluator
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 }
