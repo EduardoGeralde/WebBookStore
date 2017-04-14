d7df276aedc30da0182e131deda359118c51540f
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-04-13 21:48:15 -0300

    HttpSecurity objects configured

--- src/main/java/com/eduardoportfolio/store/conf/SecurityConfiguration.java
+++ src/main/java/com/eduardoportfolio/store/conf/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.eduardoportfolio.store.conf;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,4 +13,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 //and WebInvocationPrivilegeEvaluator
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/products/form").hasRole("ADMIN")
+		.antMatchers("/shopping/**").permitAll()
+		.antMatchers(HttpMethod.POST,"/products").hasRole("ADMIN")
+		.antMatchers("/products/**").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin();
+	}
 }
