4f3a27ec3ba0a1478865c56bd2e1f00bbee2ba1a
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-04-14 10:22:25 -0300

    UserDetailsService implemented

--- src/main/java/com/eduardoportfolio/store/conf/SecurityConfiguration.java
+++ src/main/java/com/eduardoportfolio/store/conf/SecurityConfiguration.java
@@ -1,9 +1,13 @@
 package com.eduardoportfolio.store.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 //Should be placed in top of Spring Security Configuration classes responsible for effective handle
 //the access rules.
@@ -12,6 +16,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 //our security configurations. This annotation also loads other components, like SecurityExpressionLanguage
 //and WebInvocationPrivilegeEvaluator
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -22,5 +27,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		.antMatchers("/products/**").permitAll()
 		.anyRequest().authenticated()
 		.and().formLogin();
+	}
+	
+	//In order to implement the search (for user to apply the rules) in a best way for each application, Spring Security provides the 
+	//UserDetailsService interface.
+	@Autowired
+	private UserDetailsService users;
+	//We use a overload of the configure() method that receives an AutheticationManagerBuilder object, that allow us associate a new 
+	//UserDetailService with Spring Security
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(users).passwordEncoder(new BCryptPasswordEncoder());
 	}
 }
