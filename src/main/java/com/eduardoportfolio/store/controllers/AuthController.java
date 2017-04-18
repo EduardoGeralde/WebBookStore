8661c49befc8c354bc24ec8916a83f9d72845853
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-04-18 00:44:02 -0300

    Login page controller

---
+++ src/main/java/com/eduardoportfolio/store/controllers/AuthController.java
@@ -0,0 +1,14 @@
+package com.eduardoportfolio.store.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthController {
+	
+	@RequestMapping("/login")
+	public String loginPage(){
+		return "auth/login";
+	}
+
+}
