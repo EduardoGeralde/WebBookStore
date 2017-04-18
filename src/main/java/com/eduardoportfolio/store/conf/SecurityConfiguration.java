9bbc8d8f874dae7368b4c62577473c6e0e814135
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-04-17 23:22:14 -0300

    EnableWebMvcSecurity annotation added

--- src/main/java/com/eduardoportfolio/store/conf/SecurityConfiguration.java
+++ src/main/java/com/eduardoportfolio/store/conf/SecurityConfiguration.java
@@ -4,14 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 //Should be placed in top of Spring Security Configuration classes responsible for effective handle
 //the access rules
-@EnableWebSecurity
+@EnableWebMvcSecurity
 //We inherit WebSecurityConfigurerAdapter class that already provide all infrastructure ready to start 
 //our security configurations. This annotation also loads other components, like SecurityExpressionLanguage
 //and WebInvocationPrivilegeEvaluator
