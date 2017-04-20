a484256a68ecc7366f8b43c194b71bbe556ca2af
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-04-20 09:28:27 -0300

    simple bcrypt password generator

---
+++ src/main/java/com/eduardoportfolio/store/conf/Password.java
@@ -0,0 +1,19 @@
+package com.eduardoportfolio.store.conf;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class Password {
+
+	public static void main(String[] args) {
+		
+		String password1 = "123";
+		String password2 = "456";
+		String password3 = "789";
+		
+		BCryptPasswordEncoder passwordBCrypt = new BCryptPasswordEncoder();
+		
+		System.out.println(passwordBCrypt.encode(password1));
+		System.out.println(passwordBCrypt.encode(password2));
+		System.out.println(passwordBCrypt.encode(password3));
+	}
+}
