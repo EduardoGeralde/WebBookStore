e7ff73e76881b288f25464cf8e497bf8aa63e49c
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-04-21 16:08:29 -0300

    AuthenticationSuccessHandler interface added

---
+++ src/main/java/com/eduardoportfolio/store/infra/RedirectAfterLogin.java
@@ -0,0 +1,24 @@
+package com.eduardoportfolio.store.infra;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+//We can use the successHandler method in the configure method from Security Configuration, and pass as a 
+//argument a AuthenticationSuccessHandler object (like this) and use the response method to redirect the
+//user to the location that we find most appropriate.
+public class RedirectAfterLogin implements AuthenticationSuccessHandler {
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, 
+			Authentication auth) throws IOException, ServletException {
+		
+		response.sendRedirect("/home");
+	}
+
+}
