3ec3ae7f51cd74f74cd78699af7af8033a334915
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-05-08 23:08:48 -0300

    same as before

--- src/main/java/com/eduardoportfolio/store/conf/SpringSecurityFilterConfiguration.java
+++ src/main/java/com/eduardoportfolio/store/conf/SpringSecurityFilterConfiguration.java
@@ -2,6 +2,12 @@ package com.eduardoportfolio.store.conf;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+/**
+ * 
+ * @author Eduardo Geralde Neto
+ *
+ */
+
 public class SpringSecurityFilterConfiguration 
 				extends AbstractSecurityWebApplicationInitializer{
 
