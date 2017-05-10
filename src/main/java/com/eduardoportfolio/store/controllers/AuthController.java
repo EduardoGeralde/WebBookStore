3e5acd1f3e89225dbb937395256f9a129cc7380a
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-05-09 22:19:19 -0300

    comments

--- src/main/java/com/eduardoportfolio/store/controllers/AuthController.java
+++ src/main/java/com/eduardoportfolio/store/controllers/AuthController.java
@@ -3,6 +3,14 @@ package com.eduardoportfolio.store.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 
+ * @author Eduardo Geralde Neto
+ * 
+ * 
+ *
+ */
+
 @Controller
 public class AuthController {
 	
