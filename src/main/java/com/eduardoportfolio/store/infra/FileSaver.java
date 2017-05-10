ac204f4c3f24989b8e3029e4b21de06fb993b1e0
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-05-09 22:20:24 -0300

    comments

--- src/main/java/com/eduardoportfolio/store/infra/FileSaver.java
+++ src/main/java/com/eduardoportfolio/store/infra/FileSaver.java
@@ -10,6 +10,14 @@ import com.amazonaws.AmazonClientException;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
+/**
+ * 
+ * @author Eduardo Geralde Neto
+ * 
+ * 
+ *
+ */
+
 @Component
 public class FileSaver {
 	
