d87faf2cf5fddc076e971c6268a80d402a96fc6d
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-05-18 21:20:28 -0300

    comments

--- src/main/java/com/eduardoportfolio/store/models/Role.java
+++ src/main/java/com/eduardoportfolio/store/models/Role.java
@@ -5,6 +5,13 @@ import javax.persistence.Id;
 
 import org.springframework.security.core.GrantedAuthority;
 
+/**
+ * 
+ * @author Eduardo Geralde Neto
+ * 
+ * 
+ */
+
 @Entity
 public class Role implements GrantedAuthority {
 	
